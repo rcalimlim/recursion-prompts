fb6bde98d340e99e9981bbf706d614ea2ef3d32c
Ross Calimlim <ross.calimlim@gmail.com>
2019-05-28 15:36:32 -0500

    Add functionality for summing array

--- src/recursion.js
+++ src/recursion.js
@@ -33,7 +33,20 @@ var sum = function (array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function (array) {
+	if (array.length === 0) {
+		return 0;
+	}
+	// if integer
+		// same as sum above
+	else if (Array.isArray(array[0]) === true) {
+		return arraySum(array[0]) + arraySum(array.slice(1));
+	}
+	// if array, return arraySum with array val
+	return array[0] + arraySum(array.slice(1));
 };
+
+// [ [0, 1, 2], 3, 4, 5 ]  --> 15
+// [0, 1, 2]
 
 // 4. Check if a number is even.
 var isEven = function (n) {
