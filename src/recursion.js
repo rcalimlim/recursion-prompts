ba5a11bad1d6927e219ce4458a1272cd73da012c
Ross Calimlim <ross.calimlim@gmail.com>
2019-05-28 15:27:28 -0500

    Add functionality for integer sum

--- src/recursion.js
+++ src/recursion.js
@@ -22,6 +22,12 @@ var factorial = function (n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function (array) {
+	// base case: if array length is 0, return 0
+	if (array.length === 0) {
+		return 0;
+	}
+	// else return popped + the rest of the array
+	return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
