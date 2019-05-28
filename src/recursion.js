23eee82163c6b81efd2f9939ef051ebddd1d89ba
Ross Calimlim <ross.calimlim@gmail.com>
2019-05-28 15:40:00 -0500

    Add functionality for even check

--- src/recursion.js
+++ src/recursion.js
@@ -50,6 +50,12 @@ var arraySum = function (array) {
 
 // 4. Check if a number is even.
 var isEven = function (n) {
+	if (n === 0) {
+		return true;
+	} else if (n === 1) {
+		return false;
+	}
+	return isEven(Math.abs(n) - 2);
 };
 
 // 5. Sum all integers below a given integer.
